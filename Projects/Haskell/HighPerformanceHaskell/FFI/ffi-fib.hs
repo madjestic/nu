922a72fbd21620e777a00b43806513e3e523cfe1
madjestic <madjestic13@gmail.com>
2016-12-12 20:00:34 +0100

    initial commit

---
+++ Projects/Haskell/HighPerformanceHaskell/FFI/ffi-fib.hs
@@ -0,0 +1,4 @@
+foreign import ccall
+  fib_c :: Int -> Int
+
+main = print $ fib_c 20
---
+++ Projects/Haskell/HighPerformanceHaskell/FFI/fib.c
@@ -0,0 +1,11 @@
+int fib_c(int num)
+{
+  if (num <= 2)
+    {
+      return 1;
+    }
+  else
+    {
+      return(fib_c(num -1) + fib_c(num -2));
+    }
+}
