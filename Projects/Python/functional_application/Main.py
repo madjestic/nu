46d529c2e19089237663dd206c31855745d853ea
madjestic <madjestic13@gmail.com>
2017-11-29 23:35:51 +0100

    experiment with Functional application in Python

---
+++ Projects/Python/functional_application/Main.py
@@ -0,0 +1,21 @@
+# The idea is to re-write foo(bar(baz())) as apply(foo, bar, baz)
+# >>> apply(0, addOne, addTwo)
+# 3
+# >>> apply(0, addOne, addTwo, addThree)
+# 6
+
+def addOne(x = 0):
+    return x + 1
+
+def addTwo(y = 0):
+    return y + 2
+
+def addThree(z = 0):
+    return z + 3
+
+def keep(val):
+    return val
+
+def apply(arg, f1=keep, f2=keep, f3=keep):
+    result = f1(f2(f3(arg)))
+    return result
