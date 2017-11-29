645f74e7f70ec262316b7b8ff287a4cb5b54c105
madjestic <madjestic13@gmail.com>
2017-11-29 23:48:25 +0100

    pretty-formatting

--- Projects/Python/functional_application/Main.py
+++ Projects/Python/functional_application/Main.py
@@ -1,4 +1,6 @@
-# The idea is to re-write foo(bar(baz())) as apply(foo, bar, baz)
+# The idea is to re-write foo(bar(baz(arg))) as apply(arg, foo, bar, baz)
+# Inspired by awesom haskell `$` operator: e.g.: foo $ bar $ baz $ arg
+#
 # >>> apply(0, addOne, addTwo)
 # 3
 # >>> apply(0, addOne, addTwo, addThree)
@@ -16,6 +18,9 @@ def addThree(z = 0):
 def keep(val):
     return val
 
-def apply(arg, f1=keep, f2=keep, f3=keep):
+def apply( arg
+         , f1 = keep
+         , f2 = keep
+         , f3 = keep ):
     result = f1(f2(f3(arg)))
     return result
